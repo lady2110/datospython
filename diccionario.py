90bedbcfee1ae5d472403cdb4c5cd94dc9b43cf2
Leidy <lady-2110@hotmail.com>
2022-03-11 20:57:35 -0500

    NTV003 se crea ejemplo de lista de diccionario

--- diccionario.py
+++ diccionario.py
@@ -1,10 +1,19 @@
 #Diccionario no se escribe en pluarl
 estudiante = {
 	'nombre': 'Leidy',
-	'edad': 34,
+	'edad': 30,
 	'esVoleibolista': True
 }
 #imprimir/ accedder a todas las llaves 
 #atributos de mi diccionario
 
 print(estudiante)
+#necesito acceder a un ATRIBUTO O LLAVE DEL DICCIONARIO
+print(estudiante['nombre'])
+print(estudiante.get('edad'))
+
+#recorrer un diccionario
+#y obetner sus valores
+for valor in estudiante.values():
+	print(valor)
+
---
+++ listadicionario.py
@@ -0,0 +1,12 @@
+estudiantes=[
+	{'nombre':'juan','edad':32},
+	{'nombre':'leidy','edad':30}
+]
+
+#print(estudiantes)
+
+for estudiante in estudiantes:
+	print(estudiante)
+
+#consulta
+#como hacer para agregar una nueva llave  aun diccionario
